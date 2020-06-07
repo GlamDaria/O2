c94357870f698f8b2689671a2c7d4a22e11028ea
GlamDaria <daryalutkova@yandex.ru>
2020-06-07 16:10:19 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <Header />
+    <router-view />
+  </div>
+</template>
+<script>
+import Header from "@/components/Header";
+// import styles from "@/styles/main.module.scss";
+
+export default {
+  components: {
+    Header
+  }
+};
+</script>
+
+<style lang="scss"></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="header">
+    <div class="header-logo">Logo</div>
+    <div class="header-content">Content</div>
+    <div class="header-actions">
+      <div>Личный кабинет</div>
+      <div>Корзина<</div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
+</script>
